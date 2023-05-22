249aba287ffbaf210f24e30f2826739a88671c2c
Ethan Reynolds <ethan.t.reynolds@gmail.com>
2023-05-21 22:35:20 -0600

    finish biztime steps 1-3

--- express-biztime/db.js
+++ express-biztime/db.js
@@ -1,2 +1,24 @@
 /** Database setup for BizTime. */
 
+const { Client } = require("pg");
+
+let DB_URI = {
+    user: 'etrain',
+    host: 'localhost',
+    database: '',
+    password: 'password'
+};
+
+if (process.env.NODE_ENV === "test") {
+    DB_URI = "postgresql://etrain:password@localhost/biztime_test";
+} else {
+    DB_URI = "postgresql://etrain:password@localhost/biztime";
+}
+
+let db = new Client({
+    connectionString: DB_URI
+});
+
+db.connect();
+
+module.exports = db;
